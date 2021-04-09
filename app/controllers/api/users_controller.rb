3d1fcd589075157bcdde182852fdaa23f9f510f5
Kaitlin <klewismurphy@gmail.com>
2021-04-08 20:12:17 -0500

    adds user create action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+    )
+    @user.save
     render "show.json.jb"
   end
 end
