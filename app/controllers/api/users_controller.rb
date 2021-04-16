239701b5cfd186cd4190f0af721873d2089e65fd
Kaitlin <klewismurphy@gmail.com>
2021-04-15 19:26:45 -0500

    adds password to user model, adds validations to sign up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,8 +3,14 @@ class Api::UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
     )
-    @user.save
-    render "show.json.jb"
+
+    if @user.save
+      render show.json.jb
+    else
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
+    end
   end
 end
