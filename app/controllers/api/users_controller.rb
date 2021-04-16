ae2c09ab65e3342904cccf3faf280672167ea4d6
Kaitlin <klewismurphy@gmail.com>
2021-04-15 19:56:34 -0500

    adds session controller and allows for user to log in and get jwt

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,7 +8,7 @@ class Api::UsersController < ApplicationController
     )
 
     if @user.save
-      render show.json.jb
+      render "show.json.jb"
     else
       render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
 
+    post "/sessions" => "sessions#create"
+
     get "/plants" => "plants#index"
     get "/plants/:id" => "plants#show"
   end
